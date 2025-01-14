412dd9f2b20a4208cbf8f3f81bbfd690916016ee
unknown <joseviniciusprofissional01@gmail.com>
2025-01-14 17:55:19 -0300

    Modificação de codigo

--- menu.js
+++ menu.js
@@ -3,3 +3,14 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('.cabeçalho')
     header.classList.toggle('rolagem', window.scrollY > 350)
 })
+
+let boxbuscar = document.querySelector('.btn-pesquisa')
+let lupa = document.querySelector('.img-pesquisa')
+let fechar = document.querySelector('.btn-fechar')
+
+lupa.addEventListener('click', ()=> {
+    boxbuscar.classList.add('expanção')
+})
+fechar.addEventListener('click', ()=> {
+    boxbuscar.classList.remove('expanção')
+})
